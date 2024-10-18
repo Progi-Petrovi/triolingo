00275efa43098e2010e9a5ae7715f4c6d6fc2ad4
stjepanbonic <stjepan.bonic@fer.unizg.hr>
2024-10-18 18:48:32 +0200

    initialize spring boot application (#1)

---
+++ src/main/java/com/triolingo/TriolingoApplication.java
@@ -0,0 +1,13 @@
+package com.triolingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriolingoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriolingoApplication.class, args);
+    }
+
+}
