acb91b84a58976d0db1ca3dfcf4f883aa33cb105
stjepanbonic <stjepan.bonic@fer.unizg.hr>
2024-11-06 23:07:56 +0100

    make language controller, fix constant inserting of languages upon restart

---
+++ backend/src/main/java/com/triolingo/controller/LanguageController.java
@@ -0,0 +1,24 @@
+package com.triolingo.controller;
+
+import com.triolingo.entity.language.Language;
+import com.triolingo.repository.LanguageRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/languages")
+public class LanguageController {
+    private final LanguageRepository languageRepository;
+
+    public LanguageController(LanguageRepository languageRepository) {
+        this.languageRepository = languageRepository;
+    }
+
+    @GetMapping
+    public List<Language> getAllLanguages() {
+        return languageRepository.findAll();
+    }
+}
--- backend/src/main/java/com/triolingo/entity/language/Language.java
+++ backend/src/main/java/com/triolingo/entity/language/Language.java
@@ -1,21 +1,16 @@
 package com.triolingo.entity.language;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Language {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotNull
     private String name;
