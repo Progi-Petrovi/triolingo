554b437f3296c5b55705bc5f18d8994a825559ea
Borna Gojšić <69301624+bornagojsic@users.noreply.github.com>
2024-11-06 17:48:17 +0100

    Added responsive header

---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Link } from "react-router-dom";
+
+import SiteConfig from "@/config/site";
+import { navConfig } from "@/config/header-nav";
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/icons";
+
+export function MainNav() {
+  const pathname = location.pathname;
+  console.log(pathname);
+
+  return (
+    <div className="mr-4 hidden md:flex">
+      <Link to="/" className="mr-4 flex items-center space-x-2 lg:mr-6">
+        <Icons.logo className="h-6 w-6" />
+        <span className="hidden font-bold lg:inline-block">
+          {SiteConfig.name}
+        </span>
+      </Link>
+      <nav className="flex items-center gap-4 text-sm lg:gap-6">
+        {navConfig.mainNav?.map(
+          (item) => (
+            <Link
+              to={item.href}
+              className={cn(
+                "transition-colors hover:text-foreground/80",
+                pathname === item.href ? "text-foreground" : "text-foreground/60"
+              )}
+            >
+              {item.title}
+            </Link>
+          )
+        )}
+      </nav>
+    </div>
+  )
+}
