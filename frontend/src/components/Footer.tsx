0dada901964da75377d4bdad20e02a3ab9bf4907
Borna Gojšić <69301624+bornagojsic@users.noreply.github.com>
2024-11-06 19:34:29 +0100

    Update Footer.tsx

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="border-t border-border/40 py-6 dark:border-border md:px-8 md:py-0">
+    <footer className="mt-12 border-t border-border/40 py-6 dark:border-border md:px-8 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
           Built by{" "}
