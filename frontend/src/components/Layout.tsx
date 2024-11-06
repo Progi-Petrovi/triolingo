15c2944243ae56fc2405f4a584ec1584c56edf5e
Borna Gojšić <69301624+bornagojsic@users.noreply.github.com>
2024-11-06 16:40:24 +0100

    Added basic footer and div with page id

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
 // import Header from "./Header"
-// import Footer from "./Footer"
+import { Footer } from "./Footer"
 import { Suspense } from "react";
 import { Toaster } from "./ui/toaster";
 
 export default function Layout() {
   return (
-    <>
+    <div id={location.pathname}>
       {/* <Header /> */}
       <main className="flex h-screen w-screen items-center justify-center">
         <Suspense fallback={<div>Loading...</div>}>
@@ -14,7 +14,7 @@ export default function Layout() {
         </Suspense>
         <Toaster />
       </main>
-      {/* <Footer /> */}
-    </>
+      <Footer />
+    </div>
   );
 }
