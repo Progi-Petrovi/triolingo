d03a36eb9d161c29a0194128d56c5718ac924f53
LeoTheMoldyLemon <leonardo.simunovic@gmail.com>
2024-11-08 13:00:12 +0100

    create placeholder functionality for login page (mainly for testing)

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,59 +1,92 @@
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import PathConstants from "@/routes/pathConstants";
+import { FormEvent } from "react";
 
 export default function Login() {
-  return (
-    <Card className="mx-auto max-w-sm">
-      <CardHeader>
-        <CardTitle className="text-2xl">Login</CardTitle>
-        <CardDescription>
-          Enter your email below to login to your account
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <div className="flex items-center">
-              <Label htmlFor="password">Password</Label>
-              <Link to={"#"} className="ml-auto inline-block text-sm underline">
-                Forgot your password?
-              </Link>
-            </div>
-            <Input id="password" type="password" required />
-          </div>
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
-        </div>
-        <div className="flex flex-col justify-center items-center pt-2">
-          <p>Don&apos;t have an account?</p>
-          <Link to={PathConstants.STUDENT_REGISTER} className="underline">
-            Register
-          </Link>
-          <Link to={PathConstants.TEACHER_REGISTER} className="underline">
-            Become a teacher
-          </Link>
-        </div>
-      </CardContent>
-    </Card>
-  );
+	async function submitLogin(event: FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+		const body = new FormData(event.currentTarget);
+		const res = await fetch("http://localhost:8080/login", {
+			method: "POST",
+			body,
+		});
+		if (res.status == 401) console.log("Failed to auth."); //TODO: create some kind of message for the user, the request might hold some info about the description of the error.
+	}
+
+	return (
+		<Card className="mx-auto max-w-sm">
+			<CardHeader>
+				<CardTitle className="text-2xl">Login</CardTitle>
+				<CardDescription>
+					Enter your email below to login to your account
+				</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<form className="grid gap-4" onSubmit={submitLogin}>
+					<div className="grid gap-2">
+						<Label htmlFor="email">Email</Label>
+						<Input
+							id="email"
+							name="email"
+							type="email"
+							placeholder="m@example.com"
+							required
+						/>
+					</div>
+					<div className="grid gap-2">
+						<div className="flex items-center">
+							<Label htmlFor="password">Password</Label>
+							<Link
+								to={"#"}
+								className="ml-auto inline-block text-sm underline"
+							>
+								Forgot your password?
+							</Link>
+						</div>
+						<Input
+							id="password"
+							name="password"
+							type="password"
+							required
+						/>
+					</div>
+					<div className="grid gap-2">
+						<div className="flex items-end">
+							<Input
+								id="remember-me"
+								name="remember-me"
+								type="checkbox"
+								className="w-fit h-fit mr-1"
+							/>
+							<Label htmlFor="remember-me">
+								Remember me
+							</Label>
+						</div>
+					</div>
+					<Button type="submit" className="w-full">
+						Login
+					</Button>
+				</form>
+				<div className="flex flex-col justify-center items-center pt-2">
+					<p>Don&apos;t have an account?</p>
+					<Link
+						to={PathConstants.STUDENT_REGISTER}
+						className="underline"
+					>
+						Register
+					</Link>
+					<Link
+						to={PathConstants.TEACHER_REGISTER}
+						className="underline"
+					>
+						Become a teacher
+					</Link>
+				</div>
+			</CardContent>
+		</Card>
+	);
 }
