fffebf219ef6fbb75abe694de22d1b58cdc4dddd
stjepanbonic <stjepan.bonic@fer.unizg.hr>
2024-11-15 16:06:43 +0100

    add teacher table on home page

---
+++ frontend/src/hooks/use-fetch.ts
@@ -0,0 +1,27 @@
+import PathConstants from "@/routes/pathConstants";
+import { useNavigate } from "react-router-dom";
+
+function useFetch() {
+    const navigate = useNavigate();
+
+    return async function fetchAPI(path: string, requestInit?: RequestInit) {
+        const res = await fetch(
+            new URL(path, PathConstants.API_URL),
+            requestInit
+        );
+
+        if (res.type == "opaqueredirect" || res.redirected) {
+            const url: URL = new URL(res.url);
+            if (url.origin == new URL(window.location.href).origin)
+                navigate(url.pathname + url.search);
+            else window.location.href = url.toString();
+        }
+        try {
+            return { status: res.status, body: await res.json() };
+        } catch {
+            return { status: res.status };
+        }
+    };
+}
+
+export { useFetch };
--- frontend/src/routes/pathConstants.ts
+++ frontend/src/routes/pathConstants.ts
@@ -1,9 +1,9 @@
 const PathConstants = {
-    HOME: "/",
-    LOGIN: "/login",
-    STUDENT_REGISTER: "/student/register",
-    TEACHER_REGISTER: "/teacher/register",
-    API_URL: import.meta.env.VITE_API_URL ?? "http://localhost:8080",
-}
+  HOME: "/",
+  LOGIN: "/login",
+  STUDENT_REGISTER: "/student/register",
+  TEACHER_REGISTER: "/teacher/register",
+  API_URL: import.meta.env.VITE_API_URL ?? "http://localhost:5000",
+};
 
-export default PathConstants
+export default PathConstants;
