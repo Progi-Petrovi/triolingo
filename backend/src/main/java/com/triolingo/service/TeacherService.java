bfb18691d27246fa624d2d45debe2ede83081e86
LeoTheMoldyLemon <leonardo.simunovic@gmail.com>
2024-12-17 19:52:52 +0100

    add profileImage upload service add new endpoint for uploading profile images refactor teacher property to better match existing vocabulary refactor teacher update to delete existing profile images if they are no longer in use

--- backend/src/main/java/com/triolingo/service/TeacherService.java
+++ backend/src/main/java/com/triolingo/service/TeacherService.java
@@ -6,8 +6,17 @@ import com.triolingo.entity.Teacher;
 import com.triolingo.repository.TeacherRepository;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.core.env.Environment;
 
 import javax.validation.constraints.NotNull;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,10 +28,12 @@ public class TeacherService {
 
     private final TeacherRepository teacherRepository;
     private final TeacherTranslator teacherTranslator;
+    private final Environment env;
 
-    public TeacherService(TeacherRepository teacherRepository, TeacherTranslator teacherTranslator) {
+    public TeacherService(TeacherRepository teacherRepository, TeacherTranslator teacherTranslator, Environment env) {
         this.teacherRepository = teacherRepository;
         this.teacherTranslator = teacherTranslator;
+        this.env = env;
     }
 
     public List<Teacher> listAll() {
@@ -45,6 +56,17 @@ public class TeacherService {
             throw new EntityNotFoundException("Teacher with that Id does not exist.");
 
         Teacher teacher = optionalTeacher.get();
+        if (teacher.getProfileImageHash() != null && teacherDTO.profileImageHash() == null) {
+
+            try {
+                Files.deleteIfExists(
+                        Path.of(env.getProperty("fileSystem.publicPath"),
+                                env.getProperty("fileSystem.profileImagePath"),
+                                teacher.getProfileImageHash()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         teacherTranslator.updateTeacher(teacher, teacherDTO);
         return teacherRepository.save(teacher);
     }
@@ -54,6 +76,30 @@ public class TeacherService {
         if (teacher == null)
             throw new EntityNotFoundException("Teacher with that Id does not exist.");
         teacherRepository.deleteById(id);
+    }
+
+    public String uploadProfileImage(@NotNull MultipartFile file, Teacher teacher)
+            throws NoSuchAlgorithmException, IOException {
+        if (file.getContentType() != "image/jpeg")
+            throw new IllegalArgumentException("File must be of type 'image/jpeg'");
+
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(file.getBytes());
+        String fileName = new String(hash, StandardCharsets.ISO_8859_1);
+
+        Files.copy(file.getInputStream(), Path.of(
+                env.getProperty("fileSystem.publicPath"),
+                env.getProperty("fileSystem.profileImagePath"),
+                fileName));
+        if (teacher.getProfileImageHash() != null)
+            Files.deleteIfExists(
+                    Path.of(env.getProperty("fileSystem.publicPath"),
+                            env.getProperty("fileSystem.profileImagePath"),
+                            teacher.getProfileImageHash()));
+
+        teacher.setProfileImageHash(fileName);
+        teacherRepository.save(teacher);
+        return fileName;
     }
 
 }
