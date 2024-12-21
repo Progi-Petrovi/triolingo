09b64b1971bb262dff702660205df4c532949666
LeoTheMoldyLemon <leonardo.simunovic@gmail.com>
2024-12-21 23:50:49 +0100

    add admin entity and repository fix missing student authority add admin routes to properties for rerouting

---
+++ backend/src/main/java/com/triolingo/entity/Admin.java
@@ -0,0 +1,27 @@
+package com.triolingo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Entity
+@SuperBuilder
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class Admin extends User {
+    @Override
+    public List<GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>(Arrays.asList(Role.ADMIN.getAuthority()));
+        authorities.addAll(super.getAuthorities());
+        return authorities;
+    }
+}
--- backend/src/main/java/com/triolingo/entity/Student.java
+++ backend/src/main/java/com/triolingo/entity/Student.java
@@ -10,6 +10,11 @@ import lombok.experimental.SuperBuilder;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Entity
@@ -21,14 +26,17 @@ import java.util.List;
 public class Student extends User {
     @NotEmpty
     @ManyToMany
-    @JoinTable(
-            name = "student_learning_language",
-            joinColumns = @JoinColumn(name = "student_id"),
-            inverseJoinColumns = @JoinColumn(name = "learning_language_id")
-    )
+    @JoinTable(name = "student_learning_language", joinColumns = @JoinColumn(name = "student_id"), inverseJoinColumns = @JoinColumn(name = "learning_language_id"))
     private List<LearningLanguage> learningLanguages;
     @NotNull
     private TeachingStyle preferredTeachingStyle;
     @Column(columnDefinition = "TEXT")
     private String learningGoals;
+
+    @Override
+    public List<GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>(Arrays.asList(Role.STUDENT.getAuthority()));
+        authorities.addAll(super.getAuthorities());
+        return authorities;
+    }
 }
