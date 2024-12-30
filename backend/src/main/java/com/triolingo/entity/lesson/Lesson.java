8f2ec94a6b5ebe586ff21d8a89893ce5d3919e7f
stjepanbonic <stjepan.bonic@fer.unizg.hr>
2024-12-30 17:04:43 +0100

    add Lesson and Review models

---
+++ backend/src/main/java/com/triolingo/entity/lesson/Lesson.java
@@ -0,0 +1,37 @@
+package com.triolingo.entity.lesson;
+
+import java.time.Instant;
+
+import javax.validation.constraints.*;
+
+import com.triolingo.entity.Student;
+import com.triolingo.entity.Teacher;
+import com.triolingo.entity.language.Language;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Lesson {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private Instant date;
+    @NotNull
+    private LessonStatus status;
+    @ManyToOne
+    @NotNull
+    private Teacher teacher;
+    @ManyToOne
+    @NotNull
+    private Student student;
+    @ManyToOne
+    @NotNull
+    private Language language;
+    @NotNull
+    private Double teacherPayment;
+}
