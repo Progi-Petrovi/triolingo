2fcc756bcb685e75c113e5616a16d09aa38deee7
skledaar <josip.skledar@fer.unizg.hr>
2025-01-09 00:05:40 +0100

    Add verify success page with 2s redirect to home

--- frontend/src/routes/pathConstants.ts
+++ frontend/src/routes/pathConstants.ts
@@ -3,6 +3,7 @@ const PathConstants = {
 	LOGIN: "/login",
 	STUDENT_REGISTER: "/student/register",
 	TEACHER_REGISTER: "/teacher/register",
+	VERIFY_SUCCESS: "/verify/success",
 	API_URL: import.meta.env.VITE_API_URL ?? "http://localhost:5000",
 };
 
