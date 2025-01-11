8c8daba5c99696dc6e775a8b0e128f8a9c64f791
Borna Gojšić <69301624+bornagojsic@users.noreply.github.com>
2025-01-11 01:11:11 +0100

    Update pathConstants.ts

--- frontend/src/routes/pathConstants.ts
+++ frontend/src/routes/pathConstants.ts
@@ -5,8 +5,8 @@ const PathConstants = {
     TEACHER_REGISTER: "/teacher/register",
     VERIFY_SUCCESS: "/verify/success",
     PROFILE: "/profile",
-    TEACHERPROFILE: "/teacher/:id",
     CALENDAR: "/calendar",
+    TEACHERPROFILE: "/teacher/:id",
     API_URL: import.meta.env.VITE_API_URL ?? "http://localhost:5000",
 };
 
