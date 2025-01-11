2f2c6a64ee87989c557cd20909d62d014361cdbb
stjepanbonic <stjepan.bonic@fer.unizg.hr>
2025-01-11 18:34:36 +0100

    add role endpoint

--- backend/src/main/java/com/triolingo/controller/UserController.java
+++ backend/src/main/java/com/triolingo/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.triolingo.controller;
 
+import com.dtoMapper.DtoMapper;
 import com.triolingo.dto.user.UserChangePasswordDTO;
 import com.triolingo.entity.user.User;
+import com.triolingo.repository.UserRepository;
 import com.triolingo.security.DatabaseUser;
+import com.triolingo.service.TeacherService;
 import com.triolingo.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -12,10 +15,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -39,4 +39,18 @@ public class UserController {
         userService.changePassword(user, userChangePasswordDTO);
         return ResponseEntity.ok("Password changed successfully");
     }
+
+    @GetMapping("/role")
+    @Secured("ROLE_USER")
+    @Operation(description = "Gets current user the principal is logged in as.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema()))
+    })
+    public ResponseEntity<?> getUserRoles(
+            @AuthenticationPrincipal DatabaseUser principal) {
+        User user = principal.getStoredUser();
+        return ResponseEntity.ok(user.getAuthorities());
+    }
+
 }
