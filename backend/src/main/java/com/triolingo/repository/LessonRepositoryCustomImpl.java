45c78a3deb53ba9bbd10e088053d227d39b2865f
LeoTheMoldyLemon <leonardo.simunovic@gmail.com>
2025-01-13 23:08:10 +0100

    resolve merge conflict

--- backend/src/main/java/com/triolingo/repository/LessonRepositoryCustomImpl.java
+++ backend/src/main/java/com/triolingo/repository/LessonRepositoryCustomImpl.java
@@ -2,16 +2,22 @@ package com.triolingo.repository;
 
 import java.time.Instant;
 
-import com.triolingo.entity.lesson.Lesson;import om.triolingo.entity.lesson.LessonRequest;
+import com.triolingo.entity.lesson.Lesson;
+
+import com.triolingo.entity.lesson.LessonRequest;
 import com.triolingo.entity.user.Student;
-import com.triolingo.entity.user.Teacher;import akarta .ersistence.EntityManager;
+
+import com.triolingo.entity.user.Teacher;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Join;
-import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
-public class LessonRepositoryCustomImpl i    @Pesisten cContext
+import jakarta.persistence.criteria.Predicate;
+
+public class LessonRepositoryCustomImpl implements LessonRepositoryCustom {
+    @PersistenceContext
     private EntityManager entityManager;
 
     @Override
@@ -38,7 +44,9 @@ public class LessonRepositoryCustomImpl i    @Pesisten cContext
     public boolean existsByTeacherAndAcceptedStudentAndComplete(Teacher teacher, Student student) {
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Lesson> query = builder.createQuery(Lesson.class);
-            Join<LessonRequest, Lesson> lessonJoin = requestRoot.join(LessonRequest.Fields.lesson);
+        Root<LessonRequest> requestRoot = query.from(LessonRequest.class);
+
+        Join<LessonRequest, Lesson> lessonJoin = requestRoot.join(LessonRequest.Fields.lesson);
 
         Predicate predicate = builder.and(
                 builder.equal(requestRoot.get(LessonRequest.Fields.student), student),
