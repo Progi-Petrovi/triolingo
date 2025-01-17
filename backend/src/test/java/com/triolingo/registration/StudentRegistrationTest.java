33eb35960b6694090ef4e3ce843ed25e4e593637
Petar Štika <petar.stika@fer.unizg.hr>
2025-01-17 09:42:14 +0100

    Add tests

--- backend/src/test/java/com/triolingo/registration/StudentRegistrationTest.java
+++ backend/src/test/java/com/triolingo/registration/StudentRegistrationTest.java
@@ -3,13 +3,12 @@ package com.triolingo.registration;
 import com.triolingo.dto.student.StudentCreateDTO;
 import com.triolingo.entity.TeachingStyle;
 import com.triolingo.entity.language.KnowledgeLevel;
-import com.triolingo.service.EmailService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
